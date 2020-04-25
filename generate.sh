437e064127222edceaccbd0657afbf40dda4d0b6
Ben Foxall <benfoxall@gmail.com>
2020-04-25 15:47:59 +0100

    Added JS generator

--- generate.sh
+++ generate.sh
@@ -1,6 +1,5 @@
 
-echo "Generating!!"
-
+echo "🥳 Generating /game"
 
 # https://github.com/grpc/grpc/tree/v1.28.1/examples/node/static_codegen
 
@@ -11,4 +10,13 @@ protoc \
   --grpc_out=./game/generated --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
   --plugin=protoc-gen-ts=./game/node_modules/.bin/protoc-gen-ts \
   --ts_out=./game/generated \
+  service.proto
+
+
+echo "🥰 Generating /js"
+
+mkdir -p js/generated
+protoc \
+  --js_out=import_style=commonjs,binary:./js/generated/ \
+  --grpc_out=./js/generated --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` \
   service.proto
