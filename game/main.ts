15b712229b4eb5c55c418b7ec04bf9291c349fec
Ben Foxall <benfoxall@gmail.com>
2020-04-25 14:21:13 +0100

    ALL THE STUFF

---
+++ game/main.ts
@@ -0,0 +1,170 @@
+// handle internal state of game
+
+export class Pixel {
+  x: number;
+  y: number;
+
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  distanceTo(that: Pixel): number {
+    return Math.sqrt(
+      Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+    );
+  }
+}
+
+export type Board = Pixel[];
+
+// start grpc service
+
+// logic of sending tiles to people
+
+export function splitBoard(boardToSplit: Board): Board[] {
+  let remainingPixels = [...boardToSplit];
+
+  const boards = [];
+
+  let currentBoard: Board = [remainingPixels.pop()];
+
+  while (remainingPixels.length > 0) {
+    while (true) {
+      let interacted = false;
+
+      for (let i = 0; i < remainingPixels.length; i++) {
+        const pixel = remainingPixels[i];
+        if (willPixelInteractWithBoard(currentBoard, pixel)) {
+          remainingPixels.splice(i, 1);
+
+          currentBoard.push(pixel);
+
+          interacted = true;
+        }
+      }
+
+      if (!interacted) {
+        break;
+      }
+    }
+
+    boards.push(currentBoard);
+    currentBoard = [remainingPixels.pop()];
+  }
+
+  return boards;
+}
+
+export function willPixelInteractWithBoard(
+  board: Board,
+  pixelToCheck: Pixel
+): boolean {
+  /*
+   * Check if any pixel is neighbouring using euclidean distance. The
+   * distance to any neighbour shouldn't be more than sqrt(2).
+   */
+  return board.some((p) => p.distanceTo(pixelToCheck) <= Math.SQRT2);
+}
+
+const x = new Map<Board, number>();
+
+interface IBoardUpdateState {
+  prev: Board;
+  sent: boolean;
+  result: Board;
+}
+
+interface IGameState {
+  previousState: Board;
+  boardsToProcess: IBoardUpdateState[];
+  drawnState: Board;
+}
+
+let initialState: Board = [];
+
+let state: IGameState = {
+  previousState: initialState,
+  boardsToProcess: [initialState],
+  drawnState: [],
+};
+
+const getNextAvailableBoard = async (): Promise<Board> => {
+  return board;
+};
+
+const updateBoard = (prev: Board, next: Board) => {
+  if (prev == board) {
+    board = next;
+  }
+};
+
+// grpc server stuff
+// const server = new
+import { Server, ServerCredentials } from "grpc";
+import {
+  gameOfLivesService,
+  IgameOfLivesServer,
+} from "./generated/service_grpc_pb";
+import {
+  Noop,
+  Board as pbBoard,
+  Pixel as pbPixel,
+} from "./generated/service_pb";
+
+// Set a pixel to the board
+const draw: IgameOfLivesServer["draw"] = (call) => {
+  const { x, y } = call.request.toObject();
+
+  const found = board.some((p) => p.x === x && p.y === y);
+
+  if (!found) {
+    board.push(new Pixel(x, y));
+  }
+
+  return new Noop();
+};
+
+const solve: IgameOfLivesServer["solve"] = async (call) => {
+  console.log("Connection", call.getPeer());
+
+  let maxIters = 10;
+  while (maxIters--) {
+    await new Promise((r) => setTimeout(r, 1000));
+
+    const todo = await getNextAvailableBoard();
+
+    // SEND TO CLIENT
+    const request = new pbBoard();
+    request.setPixelsList(
+      todo.map((p) => {
+        const px = new pbPixel();
+        px.setX(p.x);
+        px.setY(p.y);
+
+        return px;
+      })
+    );
+
+    call.write(request);
+
+    // wait for response
+    const response: pbBoard = await new Promise((resolve) => {
+      call.once("data", resolve);
+    });
+
+    const done: Board = response
+      .getPixelsList()
+      .map((pb) => new Pixel(pb.getX(), pb.getY()));
+
+    updateBoard(todo, done);
+  }
+};
+
+export function serverStuff() {
+  const server = new Server();
+
+  server.addService(gameOfLivesService, { solve, draw });
+  server.bind("0.0.0.0:50051", ServerCredentials.createInsecure());
+  server.start();
+}
--- game/test.ts
+++ game/test.ts
@@ -1,7 +1,112 @@
 import test from "tape";
+import { Board, Pixel, willPixelInteractWithBoard, splitBoard } from "./main";
 // import { splitBoard } from "./main";
 
-test("Example test", function (t) {
-  t.plan(1);
-  t.equal(1234, 1234);
+// https://github.com/substack/tape
+
+test("distanceTo", function (t) {
+  t.plan(4);
+
+  const origin: Pixel = new Pixel(0, 0);
+
+  t.equal(Math.sqrt(1), origin.distanceTo(new Pixel(0, 1)));
+  t.equal(Math.sqrt(1), origin.distanceTo(new Pixel(0, -1)));
+  t.equal(Math.sqrt(2), origin.distanceTo(new Pixel(1, 1)));
+
+  const far: Pixel = new Pixel(300, 300);
+
+  t.equal(Math.sqrt(2), far.distanceTo(new Pixel(301, 299)));
+});
+
+test("willPixelsInteractWithBoard", function (t) {
+  t.plan(5);
+
+  const board: Board = [
+    new Pixel(0, 0),
+    new Pixel(0, 1),
+    new Pixel(0, 2),
+    new Pixel(0, 3),
+  ];
+
+  t.equal(true, willPixelInteractWithBoard(board, new Pixel(0, 4)));
+  t.equal(false, willPixelInteractWithBoard(board, new Pixel(0, 5)));
+  t.equal(true, willPixelInteractWithBoard(board, new Pixel(1, 4)));
+  t.equal(true, willPixelInteractWithBoard(board, new Pixel(-1, -1)));
+  t.equal(false, willPixelInteractWithBoard(board, new Pixel(320, -3234)));
+});
+
+test("willPixelsInteractWithBoard", function (t) {
+  t.plan(5);
+
+  const board: Board = [
+    new Pixel(0, 0),
+    new Pixel(0, 1),
+    new Pixel(0, 2),
+    new Pixel(0, 3),
+  ];
+
+  t.ok(willPixelInteractWithBoard(board, new Pixel(0, 4)));
+  t.notOk(willPixelInteractWithBoard(board, new Pixel(0, 5)));
+  t.ok(willPixelInteractWithBoard(board, new Pixel(1, 4)));
+  t.ok(willPixelInteractWithBoard(board, new Pixel(-1, -1)));
+  t.notOk(willPixelInteractWithBoard(board, new Pixel(320, -3234)));
+});
+
+test("splitBoard", function (t) {
+  t.plan(3);
+
+  const serialse = (b: Board[]) =>
+    JSON.stringify(
+      b.map((b2) => b2.map((pix) => JSON.stringify(pix)).sort()).sort()
+    );
+
+  const areSplitBoardsSame = (b1: Board[], b2: Board[]) =>
+    serialse(b1) === serialse(b2);
+
+  t.equal(
+    serialse(
+      splitBoard([
+        new Pixel(0, 0),
+        new Pixel(0, 1),
+        new Pixel(0, 3),
+        new Pixel(0, 4),
+      ])
+    ),
+    serialse([
+      [new Pixel(0, 0), new Pixel(0, 1)],
+      [new Pixel(0, 3), new Pixel(0, 4)],
+    ])
+  );
+
+  t.equal(
+    serialse(
+      splitBoard([
+        new Pixel(0, 0),
+        new Pixel(0, 1),
+        new Pixel(0, 3),
+        new Pixel(0, 4),
+        new Pixel(0, -1),
+      ])
+    ),
+    serialse([
+      [new Pixel(0, 0), new Pixel(0, 1), new Pixel(0, -1)],
+      [new Pixel(0, 3), new Pixel(0, 4)],
+    ])
+  );
+
+  t.equal(
+    serialse(
+      splitBoard([
+        new Pixel(0, 0),
+        new Pixel(0, 1),
+        new Pixel(0, 3),
+        new Pixel(0, 4),
+        new Pixel(1, 1),
+      ])
+    ),
+    serialse([
+      [new Pixel(0, 0), new Pixel(0, 1), new Pixel(1, 1)],
+      [new Pixel(0, 3), new Pixel(0, 4)],
+    ])
+  );
 });
